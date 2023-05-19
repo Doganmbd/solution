12d7c0b44b3470d8be33a9043a3a4e3e6717be35
mbd <doganmbdogan@gmail.com>
2023-05-19 19:12:44 +0300

    1 - header ve navbar tamamlandı

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import "./header.scss";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='headerContainer'>POPULAR TOUR PLACES</div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import "./navbar.scss"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbarContainer">
+      <a href="#ABOUT US">ABOUT US</a>
+      <a href="#forYou">FOR YOU</a>
+      <a href="#SERVİCES">SERVİCES</a>
+      <a href="#BLOG">BLOG</a>
+      <a href="#VLOG">VLOG</a>
+      <a href="#CONTACT">CONTACT</a>
+      
+    </div>
   )
 }
 
